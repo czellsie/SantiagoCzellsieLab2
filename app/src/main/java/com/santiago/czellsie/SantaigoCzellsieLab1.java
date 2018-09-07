35708cc9925db6c2357a4d723d7b505d8259acac
czellsie <czellsie@czellsies-MacBook-Pro.local>
2018-09-08 07:56:39 +0800

    Initial commit

---
+++ app/src/main/java/com/santiago/czellsie/SantaigoCzellsieLab1.java
@@ -0,0 +1,13 @@
+package com.santiago.czellsie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SantaigoCzellsieLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
